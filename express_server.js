5e7385c2b3196afcf12ddcffc54a4421de095e60
elodie <elodiebouthors@hotmail.com>
2021-02-16 20:44:47 +0000

    Add a POST route that removes a URL resource: POST /urls/:shortURL/delete

--- express_server.js
+++ express_server.js
@@ -26,7 +26,7 @@ app.get("/hello", (req, res) => {
   // we can see Hello World on the page
 });
 
-//route for /urls
+//----route for /urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   console.log(templateVars);
@@ -41,35 +41,46 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//Add a GET Route to Show the Form
+//----Add a GET Route to Show the Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//Adding a Second Route
+//----Adding a Second Route
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-//Redirect any request to "/u/:shortURL" to its longURL // we type in the short URL and it brings us to the long URL website
+//----Redirect any request to "/u/:shortURL" to its longURL // we type in the short URL and it brings us to the long URL website
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+
 //--------------POST
 
-//Add a POST Route to Receive the Form Submission
+//----Add a POST Route to Receive the Form Submission
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
+  //The req.body object allows you to access data in a string or JSON object from the client side.
   // we are adding Key/Value to urlDatabase. Taking whatever is in the box called name=LongURL in urls_new
   console.log(req.body);
   // Log the POST request body to the console
   //res.send("Ok"); // Respond with 'Ok' (we will replace this)
   res.redirect(`/urls/${shortURL}`);
   // Redirect After Form Submission -taking us to line 37 "/urls/:shortURL"
+});
+
+//----Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  //The req.params object captures data based on the parameter specified in the URL.
+  delete urlDatabase[shortURL];
+
+  res.redirect(`/urls/`) //redirect the client back to the urls_index page ("/urls").
 });
 
 app.listen(PORT, () => {
