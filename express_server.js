09097e34c051fe14ed827d5b66587f4ead828644
elodie <elodiebouthors@hotmail.com>
2021-02-16 02:08:05 +0000

    adding comments and Redirect any request to /u/:shortURL to its longURL

--- express_server.js
+++ express_server.js
@@ -25,33 +25,46 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//route for /urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Add a GET Route to Show the Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Adding a Second Route
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+//Redirect any request to "/u/:shortURL" to its longURL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 //--------------POST
+
+//Add a POST Route to Receive the Form Submission
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString(); 
   urlDatabase[shortURL] = req.body.longURL; // makes a key 
   console.log(req.body);  // Log the POST request body to the console
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  res.redirect(`/urls/${shortURL}`) // taking us to line 37 "/urls/:shortURL"
+  res.redirect(`/urls/${shortURL}`) 
+  // Redirect After Form Submission -taking us to line 37 "/urls/:shortURL"
 }); 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//Generate a Random ShortURL
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 }
