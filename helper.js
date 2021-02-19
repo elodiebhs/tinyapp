fd393a6b17cb48491a0a919b94707e75e2f6adb9
elodie <elodiebouthors@hotmail.com>
2021-02-19 02:47:43 +0000

    Helper Functions Module

---
+++ helper.js
@@ -0,0 +1,9 @@
+
+module.exports.getUserByEmail = function(email, database) {
+ for (let user in database){
+   if(database[user].email === email){
+     return database[user]
+   }
+ }
+  return false;
+};
