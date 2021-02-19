dbbcc1e5b86e51f8b4a5a14ea152837397efbe69
elodie <elodiebouthors@hotmail.com>
2021-02-19 20:25:29 +0000

    cleaning code

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,13 +4,13 @@ const { getUserByEmail } = require('../helper.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -18,7 +18,7 @@ const testUsers = {
 //Write a unit test that confirms our getUserByEmail function returns a user object when it's provided with an email that exists in the database.
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
     
     assert.equal(user.id, expectedOutput);
@@ -26,11 +26,11 @@ describe('getUserByEmail', function() {
 
   //Inside the same describe statement, add another it statement to test that a non-existent email returns undefined.
 
-  it('if the email does not exist it should return undefined', function(){
+  it('if the email does not exist it should return undefined', function() {
     const user = getUserByEmail('notcorrect@1', testUsers);
     const expectedOutput = undefined;
 
     assert.equal(user.id, expectedOutput);
 
-  })
+  });
 });
